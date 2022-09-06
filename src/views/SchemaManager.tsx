6ecdf07767e043d61fda29c99e3cbc38b3761696
morita <w-morita@hscnet.co.jp>
2022-09-06 19:03:46 +0900

    ルートスキーマダウンロード時のメッセージ修正

--- src/views/SchemaManager.tsx
+++ src/views/SchemaManager.tsx
@@ -9,7 +9,7 @@ import TreeView from '@mui/lab/TreeView';
 import Box from '@mui/material/Box';
 import lodash from 'lodash';
 import { useDispatch } from 'react-redux';
-import { saveAs } from "file-saver";
+import { saveAs } from 'file-saver';
 import CustomTreeItem from '../components/Schemamanager/CustomTreeItem';
 import { UserMenu } from '../components/common/UserMenu';
 import { SystemMenu } from '../components/common/SystemMenu';
@@ -712,18 +712,25 @@ const SchemaManager = () => {
                           className="normal-button nomargin"
                           title="スキーマファイルをダウンロードします"
                           onClick={() => {
-                            const jsonStr = JSON.stringify(selectedSchemaInfo.document_schema, null, 2);
+                            const jsonStr = JSON.stringify(
+                              selectedSchemaInfo.document_schema,
+                              null,
+                              2
+                            );
                             const blob = new Blob([jsonStr], {
                               type: 'application/json',
-                            })
+                            });
                             let fileName = '';
-                            if(selectedSchemaInfo.schema_id_string) {
-                              fileName = selectedSchemaInfo.schema_id_string.replace(/^\/+|\/+$/g, '').replace(/\//g, '_');
+                            if (selectedSchemaInfo.schema_id_string) {
+                              // ファイル名はスキーマIDの先頭と末尾から"/"を除き、残りの"/"は"_"へ変換する
+                              fileName = selectedSchemaInfo.schema_id_string
+                                .replace(/^\/+|\/+$/g, '')
+                                .replace(/\//g, '_');
                             }
-                            if(fileName) {
-                              saveAs(blob, `schemafile_${fileName}.json`);
+                            if (fileName) {
+                              saveAs(blob, `${fileName}.json`);
                             } else {
-                              alert('ルートスキーマはダウンロードできません。');
+                              alert('ダウンロード不可なスキーマです。');
                             }
                           }}
                         >

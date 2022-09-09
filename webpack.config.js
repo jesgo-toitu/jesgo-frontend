ab379df35ee6ec7def888e6b2a1c41dacb006daf
morita <w-morita@hscnet.co.jp>
2022-09-09 10:54:06 +0900

    フロント側package.jsonなどの精査

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     // publicPath: '/public/' // (*)
-
   },
   resolve: {
     modules: [ "./node_modules" ],
